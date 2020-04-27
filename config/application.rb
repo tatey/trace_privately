755903c747f28fea32983f1d8b6400e6e6641e50
Tate Johnson <tate@tatey.com>
2020-04-27 21:20:26 +1000

    Anonymize the client's IP by transforming it with a keyed hash function.

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module TracePrivately
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    # Anonymize the client's IP by transforming it with a keyed hash function.
+    config.middleware.insert_after ActionDispatch::RemoteIp, IpAnonymizer::HashIp, key: Rails.application.secret_key_base
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,11 +1,15 @@
 Rack::Attack.throttle("Noisy requests for access grants", limit: 3, period: 15.minute) do |request|
-  request.ip if request.path == "/api/auth" && request.post?
+  anonymize(request.ip) if request.path == "/api/auth" && request.post?
 end
 
 Rack::Attack.throttle("Noisy API clients", limit: 10, period: 1.minute) do |request|
-  request.ip if request.path.starts_with?("/api")
+  anonymize(request.ip) if request.path.starts_with?("/api")
 end
 
 Rack::Attack.throttle("Noisy web clients", limit: 30, period: 1.minute) do |request|
-  request.ip if request.path == "/" || request.path.starts_with?("/admin")
+  anonymize(request.ip) if request.path == "/" || request.path.starts_with?("/admin")
+end
+
+def anonymize(ip)
+  IpAnonymizer.hash_ip(ip, key: Rails.application.secret_key_base)
 end
